1f422675d5f0294534136006a6cab58ec89e1c26
Levy Araujo <levy4raujo@gmail.com>
2023-05-26 11:19:36 -0300

    [Req 01 Concluído]
    Criando as migrations.

---
+++ src/migrations/20230526130958-create-users.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      displaName: {
+        allowNull: false,
+        field: 'display_name',
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('users');
+  }
+};
