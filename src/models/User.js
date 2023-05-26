948dd7fef1a66ae6f0e71cfa4063efa70a72009d
Levy Araujo <levy4raujo@gmail.com>
2023-05-26 13:15:25 -0300

    [Req 02 Concluído]
    Criando model User.

---
+++ src/models/User.js
@@ -0,0 +1,34 @@
+const User = (sequelize, DataTypes) => {
+  const user = sequelize.define('User', {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      display_name: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      image: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+  },
+  {
+    tableName: 'users'
+  }
+  );
+
+  return user;
+};
+
+module.exports = User;
