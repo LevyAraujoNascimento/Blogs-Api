79f325cd35c1ce73318f55c88fbfaedd5e4bd0a4
Levy Araujo <levy4raujo@gmail.com>
2023-05-29 12:39:43 -0300

    [Req 07 Concluído]
    Criando o Model para Category.

---
+++ src/models/Category.js
@@ -0,0 +1,24 @@
+const Category = (sequelize, DataTypes) => {
+    const category = sequelize.define('Category', {
+      id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.INTEGER
+        },
+        name: {
+          allowNull: false,
+          type: DataTypes.STRING
+        },
+    },
+    {
+      tableName: 'categories',
+      timestamps: false,
+      underscored: true,
+    }
+    );
+  
+    return category;
+  };
+  
+  module.exports = Category;
